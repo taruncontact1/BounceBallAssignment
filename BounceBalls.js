f08ddf53a2b16e138015afc8f5d8242f6d62ff23
tarun <taruncontact1@gmail.com>
2020-02-10 15:14:53 +0000

    AddedSimpleBallMovement
    based on velocity vector in x and y direction ball's location gets updated

--- BounceBalls.js
+++ BounceBalls.js
@@ -3,25 +3,42 @@ class BouncingBalls{
   // initialize the canvas and adds an event listener on mouse click
   constructor(canvasId)
   {
+    this.balls = [];
     this.canvas = document.getElementById(canvasId);
     this.canvasContext = this.canvas.getContext('2d');
     this.canvas.addEventListener("click", this.addBall.bind(this), false);
+    this.startAnimation();
   }
+  //clears the canvas and renders the balls at new updated location
+  startAnimation()
+  {
+    //clears the canvas
+    this.canvasContext.clearRect(0, 0, canvas.width, canvas.height);
 
+    //draw ball and update ball's location
+    for(var i = 0; i < this.balls.length; i++)
+    {
+      this.balls[i].draw();
+      this.balls[i].move();
+    }
+    requestAnimationFrame(this.startAnimation.bind(this));
+  }
   //creates and renders ball on click
   addBall(event)
   {
     var x = event.clientX;
     var y = event.clientY;
-    var ball = new Ball(this.canvas, x, y,'black', 20);
-    ball.draw();
+    var ball = new Ball(this.canvas, x, y,'black', 20, 5, 5);
+    this.balls.push(ball);
   }
 }
 
 class Ball
 {
-  constructor(canvas, x, y, color, radius)
+  constructor(canvas, x, y, color, radius, velocityX, velocityY)
   {
+    this.velocityX = velocityX;
+    this.velocityY = velocityY;
     this.canvas = canvas;
     this.color = color;
     this.radius = radius;
@@ -36,6 +53,12 @@ class Ball
     context.fillStyle = this.color;
     context.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
     context.fill();
+  }
+  // updates ball coordinate based on velocity
+  move()
+  {
+    this.x += this.velocityX;
+    this.y += this.velocityY;
   }
 }
 
